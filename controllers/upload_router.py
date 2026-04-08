a4151915ccad3178740c5063bb943072ed51cfee
Jashhvanth Tamilselvan Kunthavai <jashu.tamil@gmail.com>
2026-04-08 09:58:25 -0400

    It is done
    now I want to push some stuff to azure and see how well that works.

--- controllers/upload_router.py
+++ controllers/upload_router.py
@@ -7,12 +7,9 @@ from movie_rec.data_processing import save_feedback
 
 router = APIRouter(prefix="/api/v1/upload")
 
-@router.post("/send_feedback/")
-async def file_upload(data: dict = Body(...)):
-    print(data)
-    feedback = data[0]
-    user = data[1]
-    save_feedback(feedback, user)
+@router.post("/send_feedback/{user}")
+async def file_upload(user: str, data: dict = Body(...)):
+    save_feedback(data, user)
 
     return JSONResponse(status_code=status.HTTP_200_OK, content='File uploaded successfully!')
 
